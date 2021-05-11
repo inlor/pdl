d178aedd718e79ae522c9611adccb8ede27dd5fc
Lucian Petic <lucian.petic14@gmail.com>
2021-05-11 22:33:07 +0200

    Update composer.json and symfony.lock

--- config/bundles.php
+++ config/bundles.php
@@ -8,9 +8,9 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    // Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
-    Symfony\Bundle\DebugBundle\DebugBundle::class => ['all' => true], // dev
-    Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true], // dev
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
